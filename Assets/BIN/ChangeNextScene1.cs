37b6317024444579013d862f79c7e4838804fd15
Tuanbaga0107 <116897302+TunTunn0107@users.noreply.github.com>
2024-03-18 22:42:42 +0700

    aaa

---
+++ Assets/BIN/ChangeNextScene1.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeNextScene : MonoBehaviour
+{
+    public int sceneBuildIndex;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            //this is to go to next scene
+            SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
+        }
+    }
+} 
---
+++ Assets/Script/CollectDocument.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public int requiredDocument = 3
+
+public class CollectDocument : MonoBehaviour
+{
+    public void CollectDocument()
+    {
+        CollectDocument++
+        {
+            if(CollectDocument)
+        } 
+    }
+   
+}
