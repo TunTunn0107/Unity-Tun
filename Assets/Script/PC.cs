025caa7d72a86a98c9446151d3cedb468b0b2e82
Tuanbaga0107 <116897302+TunTunn0107@users.noreply.github.com>
2024-03-19 12:14:16 +0700

    Fixing change scene

--- Assets/Script/PC.cs
+++ Assets/Script/PC.cs
@@ -5,37 +5,37 @@ using UnityEngine.SceneManagement;
 
 public class PC : MonoBehaviour
 {
+    // Create a public variable to store the name of the scene you want to transition to
+    public string nextSceneName;
+
     //public CollectDocument collectedDocument;
     public CollectDocument[] collectibleDocuments;
-    void Start()
-    {
-        
-    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (other.CompareTag("Player"))
         {
-
-            DocumentManager documentManager = FindObjectOfType<DocumentManager>();
-            if (documentManager != null && documentManager.AreAllDocumentsCollected())
+            // Check if all documents have been collected
+            if (AreAllDocumentsCollected())
             {
-                SceneManager.LoadScene("PC_Screen");
+                // If all documents have been collected, transition to the next scene
+                SceneManager.LoadScene(nextSceneName);
             }
         }
     }
 
-    //private bool AreAllDocumentsCollected()
-    //{
-        //foreach (CollectDocument doc in collectibleDocuments)
-       // {
-           // if (!doc.AreAllDocumentsCollected())
-         //   {
-         //       return false;
-        //    }
-       // }
-        //return true;
-  //  }
+    private bool AreAllDocumentsCollected()
+    {
+        // Check each collected document
+        foreach (CollectDocument doc in collectibleDocuments)
+        {
+            // If any document is not collected, return false
+            if (!doc.isCollected)
+            {
+                return false;
+            }
+        }
+        // If all documents are collected, return true
+        return true;
     }
-
-
+}
